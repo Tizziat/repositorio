9395ea73f9c5fcf83e7f1788dbae2e09beb6bbe2
“Tiziano <tizzitobares@gmail.com>
2026-08-20 19:21:07 -0300

    Presentacion
    añadi mi nombre

--- src/repositorio/Repositorio.java
+++ src/repositorio/Repositorio.java
@@ -1,20 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package repositorio;
 
-/**
- *
- * @author tizzi
- */
+
 public class Repositorio {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        System.out.println("grupo 15");
+        System.out.println("Tiziano Tobares");
     }
     
 }
